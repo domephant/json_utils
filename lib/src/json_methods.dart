f40ad9a9a502acd461311dcd6b595fa9073b32e4
domephant <66464989+domephant@users.noreply.github.com>
2022-01-06 21:57:22 +0100

    changed json getters to methods and added regions

--- lib/src/json_methods.dart
+++ lib/src/json_methods.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
 class JsonUtils {
+  // #region private methods
   static Future<File> get _localFile async {
     final path = await _getPath;
     return File('$path/shoppinglist.json');
@@ -12,14 +13,16 @@ class JsonUtils {
     Directory dir = await getApplicationDocumentsDirectory();
     return dir.path;
   }
+  //#endregion
 
+  //#region public methods
   static writeJson(List list) async {
     String jsonList = jsonEncode(list);
     final file = await _localFile;
     file.writeAsString(jsonList);
   }
 
-  static Future<List> get readJson async {
+  static Future<List> readJson() async {
     final file = await _localFile;
     if (await file.exists()) {
       String fileText = await file.readAsString();
@@ -32,28 +35,28 @@ class JsonUtils {
     return List.empty();
   }
 
-  static Future<List> get getListfromJson async {
-    Iterable temp = await JsonUtils.readJson;
+  static Future<List> getListfromJson() async {
+    Iterable temp = await JsonUtils.readJson();
     List returnList = List.empty();
     returnList = List.from(temp.map((model) => model));
     return returnList;
   }
 
   static void appendToList(List list) async {
-    List fileList = await JsonUtils.getListfromJson;
+    List fileList = await JsonUtils.getListfromJson();
     fileList.addAll(list);
     JsonUtils.writeJson(fileList);
   }
 
   static void removeElement(var elementToRemove) async {
-    List fileList = await JsonUtils.getListfromJson;
+    List fileList = await JsonUtils.getListfromJson();
     fileList.removeWhere(
         (element) => element.ingredient.name == elementToRemove.name);
     JsonUtils.writeJson(fileList);
   }
 
   static void changeValuefromListAt(var element) async {
-    List fileList = await JsonUtils.getListfromJson;
+    List fileList = await JsonUtils.getListfromJson();
     int searchedIndex = fileList.indexWhere(
         (element) => element.ingredient.name == element.ingredient.name);
     fileList[searchedIndex] = element;
@@ -62,7 +65,7 @@ class JsonUtils {
 
   static Future<bool> listContainsElement(var elementToCheck) async {
     List fileList = List.empty();
-    bool returnValue = await JsonUtils.getListfromJson.then((value) {
+    bool returnValue = await JsonUtils.getListfromJson().then((value) {
       fileList = value;
       if (fileList.any((element) =>
           element.ingredient.name == elementToCheck.ingredient.name)) {
@@ -75,13 +78,14 @@ class JsonUtils {
   }
 
   static Future<dynamic> getElementAtIngredient(var elementToCheck) async {
-    List fileList = await JsonUtils.getListfromJson;
+    List fileList = await JsonUtils.getListfromJson();
     return fileList[fileList.indexWhere(
         (element) => element.ingredient.name == elementToCheck.name)];
   }
 
   static Future<dynamic> getElementAtIndex(int i) async {
-    List fileList = await JsonUtils.getListfromJson;
+    List fileList = await JsonUtils.getListfromJson();
     return fileList[i];
   }
+  //#endregion
 }
